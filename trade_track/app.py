64d4032e3db87fc93c6ab5b09a32d4cb0b86386a
SiphiweAI <aisiphiwe@gmail.com>
2025-11-03 17:41:26 +0200

    fixed import issues

--- trade_track/app.py
+++ trade_track/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 import logging
 import os
-from tasks import process_trade
-from log_config import setup_logging
-from helper_funcs import validate_env_vars
+from trade_track.tasks import process_trade
+from trade_track.log_config import setup_logging
+from trade_track.helper_funcs import validate_env_vars
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
